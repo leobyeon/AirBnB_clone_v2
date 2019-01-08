a568a60f6f2bde507508f665bee5b01cac563c62
leobyeon <386@holbertonschool.com>
2019-01-08 21:47:09 +0000

    Removed cascade

--- models/user.py
+++ models/user.py
@@ -20,4 +20,4 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
 
-    places = relationship("Place", cascade="all,delete", backref="users")
+    places = relationship("Place", passive_deletes=True, backref="user")
