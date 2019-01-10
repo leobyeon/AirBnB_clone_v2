1f559fb2e2ec71070d9eb90d257758503b88ab92
leobyeon <386@holbertonschool.com>
2019-01-10 01:40:45 +0000

    wrapped getters and setters in env check

--- models/place.py
+++ models/place.py
@@ -5,7 +5,6 @@ from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
 import os
 import models
 from sqlalchemy.orm import relationship
-from models.amenity import Amenity
 
 
 place_amenity = Table(
@@ -34,17 +33,14 @@ class Place(BaseModel, Base):
         amenity_ids: list of Amenity ids
     """
     __tablename__ = "places"
-    metadata = Base.metadata
 
-    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
-        amenities = relationship(
-                "Amenity", secondary=place_amenity,
-                backref="place_amenities", viewonly=False)
-        reviews = relationship("Review", cascade="delete", backref="place")
-    else:
+    amenities = relationship(
+            "Amenity", secondary=place_amenity, backref="place_amenities", viewonly=False)
+    reviews = relationship("Review", cascade="delete", backref="place")
+    """else:
         amenities = ""
         reviews = ""
-
+    """
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
@@ -57,33 +53,35 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    @property
-    def amenities(self):
-        """ gets the list of amenities """
-        a_ls = []
-        a_objs = models.storage.all(Amenity)
-        for obj_key, obj_val in a_objs.items():
-            if obj_val.id in self.amenity_ids:
-                a_ls.append(obj)
-        return a_ls
+    if os.getenv("HBNB_TYPE_STORAGE") == 'file':
 
-    @amenities.setter
-    def amenities(self, obj):
-        """ handles append method for amenities """
-        if isinstance(obj, Amenity):
-            self.amenity_ids.append(obj.id)
+        @property
+        def amenities(self):
+            """ gets the list of amenities """
+            a_ls = []
+            a_objs = models.storage.all(models.Amenity)
+            for obj_key, obj_val in a_objs.items():
+                if obj_val.id in self.amenity_ids:
+                    a_ls.append(obj)
+            return a_ls
 
-    @property
-    def reviews(self):
-        """
-        getter attribute 'reviews' that returns a list of Review
-        instances with place_id equal to Place.id
-        """
-        rev_objs = models.storage.all("Review")
-        place_objs = models.storage.all("Place")
-        rev_ls = []
-        for name_id, value in rev_objs.items():
-            for pls_name_id in place_objs.keys():
-                if value.place_id == pls_name_id.split('.')[1]:
-                    rev_ls.append(value)
-        return rev_ls
+        @amenities.setter
+        def amenities(self, obj):
+            """ handles append method for amenities """
+            if isinstance(obj, models.Amenity):
+                self.amenity_ids.append(obj.id)
+
+        @property
+        def reviews(self):
+            """
+            getter attribute 'reviews' that returns a list of Review
+            instances with place_id equal to Place.id
+            """
+            rev_objs = models.storage.all(models.Review)
+            place_objs = models.storage.all(models.Place)
+            rev_ls = []
+            for name_id, value in rev_objs.items():
+                for pls_name_id in place_objs.keys():
+                    if value.place_id == pls_name_id.split('.')[1]:
+                        rev_ls.append(value)
+            return rev_ls
