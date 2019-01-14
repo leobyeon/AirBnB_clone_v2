cd0ced99071b95a0acc1f706918bacd4ee011775
leobyeon <386@holbertonschool.com>
2019-01-14 23:55:25 +0000

    Removed /versions/

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,18 +3,17 @@
 a Fabric script that generates a .tgz archive
 from the contents of the web_static folder
 """
+from fabric.api import *
+from datetime import datetime
 
 
 def do_pack():
     """ creates a tarball """
-    from fabric.api import local
-    from datetime import datetime
-
     thetime = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "/versions/web_static_{}.tgz".format(thetime)
+    filename = "web_static_{}.tgz".format(thetime)
 
     local("mkdir -p versions")
-    process = local("tar -cvzf {} ./web_static".format(filename))
+    process = local("tar -cvzf {} web_static".format(filename))
 
     if process.succeeded:
         return filename
