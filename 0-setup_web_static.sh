88a5b00da58857ffb757fe835344d776d6e59474
leobyeon <386@holbertonschool.com>
2019-01-14 22:36:34 +0000

    Initial commit

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# sets up the web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+sudo mkdir -p data
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+sudo touch /data/web_static/releases/test/index.html
+echo "<html><head>Test</head></html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/current /data/web_static/releases/test
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '41i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
