c0dc35c62123ccf6a89fce13efbbef02dd736c4c
leobyeon <386@holbertonschool.com>
2019-01-15 04:29:34 +0000

    Added os.path.isfile as a check

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ distributes an archive to web servers """
 from fabric.api import *
+import os.path
 env.hosts = ['35.243.251.251', '34.73.23.24']
 
 
 def do_deploy(archive_path):
     """ deploys the archive """
 
-    if not archive_path:
+    if not os.path.isfile(archive_path):
         return False
     try:
         filename = archive_path.split("/")[1]
@@ -18,7 +19,7 @@ def do_deploy(archive_path):
         run("tar -xzf /tmp/{} -C {}releases/{}/".format(
             filename, datapath, no_ext))
         run("rm /tmp/{}".format(filename))
-        run("mv {}releases/{}/web_static/* {}releases/{}".format(
+        run("mv {}releases/{}/web_static/* {}releases/{}/".format(
             datapath, no_ext, datapath, no_ext))
         run("rm -rf {}releases/{}/web_static".format(datapath, no_ext))
         run("rm -rf {}current".format(datapath))
